e2722e057bb675c535a3f4c7924c036158432611
Daniel German <danielchuksgerman@gmail.com>
2023-10-03 05:07:57 -0700

    3-print_alphabt.py fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for character in range(97, 123):
     if character != 101 and character != 113:
-        print(chr(character), end='')
+        print("{}".format(chr(character)), end='')
