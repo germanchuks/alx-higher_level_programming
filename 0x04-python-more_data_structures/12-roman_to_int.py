ddc8accf15f7d2ebc4c62b1da59ddf56da8e5d59
Daniel German <danielchuksgerman@gmail.com>
2023-10-11 09:10:25 -0700

    12-roman_to_int.py fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not str(roman_string) or not roman_string:
+    if not roman_string or not isinstance(roman_string, str):
         return 0
+
     roman_styles = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
                     'D': 500, 'M': 1000}
     roman_num = 0
     prev_value = 0
+
+    roman_string = roman_string.upper()
 
     for i in reversed(roman_string):
         if i in roman_styles:
