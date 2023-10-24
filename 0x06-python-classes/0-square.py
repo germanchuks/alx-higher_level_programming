5214393b22f1fe2a653069a4a82f9420dde00553
Daniel German <danielchuksgerman@gmail.com>
2023-10-24 07:16:50 -0700

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 class Square:
-    """This class defines a Square"""
-    pass
+    """
+    This class represents a square.
+
+    A square is a geometric shape with four equal sides and four right angles.
+
+    Methods
+        __init__(self): Initializes a new Square instance.
+    """
+    def __init__(self):
+        pass
