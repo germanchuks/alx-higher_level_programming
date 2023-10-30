1cbfd26c492238350161c9c2b5a9072a43e4c21d
Daniel German <danielchuksgerman@gmail.com>
2023-10-30 02:24:18 -0700

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for a Rectangle"""
+
+
+class Rectangle:
+    """
+    This class represents a rectangle.
+
+    A square is a geometric shape with four sides and four right angles.
+    The opposite sides of a rectangle are parallel and of equal length.
+
+    """
+    pass
