92d6cbdb4350ed238c98f089749f34862ca47678
Daniel German <danielchuksgerman@gmail.com>
2023-11-06 03:45:53 -0800

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,7 +4,8 @@
 
 class MyList(list):
     """
-    MyList is a custom list class that inherits from the built-in `list` class.
+    MyList is a custom list class that inherits from the built-in `list`
+    class.
 
     Public Methods:
         - print_sorted(): Prints the elements of the list in ascending order.
