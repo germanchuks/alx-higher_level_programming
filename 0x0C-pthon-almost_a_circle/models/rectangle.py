2bc3094eb7b4a04ba72a3002833c3d7df8c264da
Daniel German <danielchuksgerman@gmail.com>
2023-11-09 09:05:07 -0800

    First Rectangle

--- 0x0C-pthon-almost_a_circle/models/rectangle.py
+++ 0x0C-pthon-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Module for Rectangle inheritted from Base"""
-Base = __import__('base').Base
+from base import Base
 
 
 class Rectangle(Base):
@@ -24,43 +24,42 @@ class Rectangle(Base):
         self.__y = y
         super().__init__(id)
 
-        @property
-        def width(self):
-            """Gets the width of the rectangle."""
-            return (self.__width)
+    @property
+    def width(self):
+        """Gets the width of the rectangle."""
+        return (self.__width)
 
-        @width.setter
-        def width(self, value):
-            """Sets the width of the rectangle."""
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """Sets the width of the rectangle."""
+        self.__width = value
 
-        @property
-        def height(self):
-            """Gets the height of the rectangle."""
-            return (self.__height)
+    @property
+    def height(self):
+        """Gets the height of the rectangle."""
+        return (self.__height)
 
-        @height.setter
-        def height(self, value):
-            """Sets the height of the rectangle."""
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """Sets the height of the rectangle."""
+        self.__height = value
 
-        @property
-        def x(self):
-            """Gets the x-coordinate of the rectangle."""
-            return (self.__x)
+    @property
+    def x(self):
+        """Gets the x-coordinate of the rectangle."""
+        return (self.__x)
 
-        @x.setter
-        def x(self, value):
-            """Sets the x-coordinate of the rectangle."""
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        """Sets the x-coordinate of the rectangle."""
+        self.__x = value
 
-        @property
-        def y(self):
-            """Gets the y-coordinate of the rectangle."""
-            return (self.__y)
+    @property
+    def y(self):
+        """Gets the y-coordinate of the rectangle."""
+        return (self.__y)
 
-        @y.setter
-        def y(self, value):
-            """Sets the y-coordinate of the rectangle."""
-            self.__y = value
-
+    @y.setter
+    def y(self, value):
+        """Sets the y-coordinate of the rectangle."""
+        self.__y = value
