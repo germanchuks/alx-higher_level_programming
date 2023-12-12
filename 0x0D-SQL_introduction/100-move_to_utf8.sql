162f212412cef73ab5cbdf95165a150bdba32634
Daniel German <danielchuksgerman@gmail.com>
2023-12-12 07:25:51 -0800

    Go to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,8 @@
+-- Converts database `hbtn_0c_0` to UTF8.
+-- Converts the `first_table` table to utf8mb4.
+-- Converts the field `name` in `first_table` to utf8mb4.
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE hbtn_0c_0.first_table MODIFY COLUMN name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
