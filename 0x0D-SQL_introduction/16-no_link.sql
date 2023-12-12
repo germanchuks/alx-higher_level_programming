76a454a3be4577deac76991feea09d861b65a80d
Daniel German <danielchuksgerman@gmail.com>
2023-12-12 07:16:26 -0800

    Say my name - fix

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- Lists all records of the table `second_table`.
 -- Rows without a name value are not listed.
-SELECT * FROM second_table
+SELECT score, name FROM second_table
     WHERE name IS NOT NULL AND name != ''
     ORDER BY score DESC;
