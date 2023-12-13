5d627b51ca24bf17dbed7f0f5bef65bee5cc29ba
Daniel German <danielchuksgerman@gmail.com>
2023-12-13 05:02:51 -0800

    Read user - fix 4

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,13 +1,11 @@
--- Creates the database `htbn_0d_2` and the user `user_0d_2` if it does not exists.
--- Grants only `SELECT` privilege to `user_0d_2` in `hbtn_0d_2` database.
+-- Creates database `htbn_0d_2` and user `user_0d_2`,
+-- if they do not exists.
+-- Grants `SELECT` privilege to `user_0d_2` in `hbtn_0d_2`.
 CREATE DATABASE
     IF NOT EXISTS htbn_0d_2;
 CREATE USER
     IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
-GRANT USAGE
-    ON *.*
-    TO 'user_0d_2'@'localhost';
 GRANT SELECT
     ON htbn_0d_2.*
     TO 'user_0d_2'@'localhost';
