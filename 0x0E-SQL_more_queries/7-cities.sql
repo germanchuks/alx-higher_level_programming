8ed6de3b857c5af5a964377fc11c168f9ff1cbaa
Daniel German <danielchuksgerman@gmail.com>
2023-12-13 06:05:55 -0800

    Cities table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- Creates the database `hbtn_0d_usa` and table `cities` if they do not exists.
+CREATE DATABASE
+    IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id),
+    name VARCHAR(256) NOT NULL
+);
