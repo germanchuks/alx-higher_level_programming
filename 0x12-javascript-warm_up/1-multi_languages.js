8803dc9b36371bfad2e3500f7570d171a059b524
Daniel German <danielchuksgerman@gmail.com>
2024-01-08 10:42:10 -0800

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-/* Prints 3 lines:
-The first line: “C is fun”
-The second line: “Python is cool”
-The third line: “JavaScript is amazing”
-*/
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
