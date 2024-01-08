e9335d952dd79c7dff934588adcd85113b4c480c
Daniel German <danielchuksgerman@gmail.com>
2024-01-08 11:27:34 -0800

    Create a sentence

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// Prints two arguments passed to it, in the following format: “ is ”
+
+const inputStrings = process.argv[2] + ' is ' + process.argv[3];
+console.log(inputStrings);
