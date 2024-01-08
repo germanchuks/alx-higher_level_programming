b80bb53666e76b0eaebbd89b808031d00257bf06
Daniel German <danielchuksgerman@gmail.com>
2024-01-08 12:52:41 -0800

    Call me Moby

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Executes x times in a function.
+
+function callMeMoby(x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+}
+
+module.exports = { callMeMoby };
