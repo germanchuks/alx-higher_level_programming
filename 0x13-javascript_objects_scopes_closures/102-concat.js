f1ace4eec473a91f73fa0323840c2402e62b740a
Daniel German <danielchuksgerman@gmail.com>
2024-01-09 11:49:23 -0800

    Concat files fix

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -7,23 +7,9 @@ The third argument is the file path of the destination
 
 const fs = require('fs');
 
-if (process.argv.length !== 5) {
-  console.log('Usage: ./102-concat.js fileA fileB fileC');
-  process.exit(1);
-}
+const contentA = fs.readFileSync(process.argv[2]);
+const contentB = fs.readFileSync(process.argv[3]);
 
-const fileAPath = process.argv[2];
-const fileBPath = process.argv[3];
-const destPath = process.argv[4];
+const concatenatedContent = contentA + contentB;
 
-try {
-  const contentA = fs.readFileSync(fileAPath);
-  const contentB = fs.readFileSync(fileBPath);
-
-  const concatenatedContent = contentA + '\n' + contentB;
-
-  fs.writeFileSync(destPath, concatenatedContent);
-} catch (err) {
-  console.log(err.message);
-  process.exit(1);
-}
+fs.writeFileSync(process.argv[4], concatenatedContent);
