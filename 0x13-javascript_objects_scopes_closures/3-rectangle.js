c5e62b7ead7b14cc0c28fe4db5fad42a0fded510
Daniel German <danielchuksgerman@gmail.com>
2024-01-09 10:23:28 -0800

    Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+/* Defines a rectangle:
+Constructor takes 2 arguments w and h
+Initialize the instance attribute width with the value of w
+Initialize the instance attribute height with the value of h
+If w or h is equal to 0 or not a positive integer, create an empty object
+Contains an instance method called print() that prints the rectangle using the character X
+*/
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return {};
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Rectangle;
